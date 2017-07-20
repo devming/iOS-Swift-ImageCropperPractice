192089d1f072064bc68ff6ce66b9fa1af70e2af0
Minki <devming@Minkiui-MacBook-Pro.local>
2017-07-20 17:19:41 +0900

    initial commit

---
+++ ImageCropper/CropAreaView.swift
@@ -0,0 +1,15 @@
+//
+//  CropAreaView.swift
+//  ImageCropper
+//
+//  Created by Minki on 2017. 7. 20..
+//  Copyright © 2017년 Minki. All rights reserved.
+//
+
+import UIKit
+
+class CropAreaView: UIView {
+    override func point(inside point: CGPoint, with event:   UIEvent?) -> Bool {
+        return false
+    }
+}
